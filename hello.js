3bd4e06bab8e216cd027376015a2370985c626b2
Senshipknl1019 <pknlasagas@gmail.com>
2025-08-28 10:09:16 +0800

    added hello world

--- hello.js
+++ hello.js
@@ -1,14 +1,1 @@
-console.log ("HELLO WORLD");
-
-num1 = 2;
-num2 = 3;
-
-sum = num1 + num2;
-diff = num1 - num2;
-prod = num1 * num2;
-quo = num1 / num2;
-
-console.log(sum); ("the sum of " + num1 + " and " + num2 + " is " + sum);
-console.log(diff); ("the difference of " + num1 + " and " + num2 + " is " + diff);
-console.log(prod); ("the product of " + num1 + " and " + num2 + " is " + prod);
-console.log(quo); ("the quotient of " + num1 + " and " + num2 + " is " + quo);
+console.log("hello world");
