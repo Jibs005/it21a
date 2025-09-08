b34008c8f5cfd59c73015bc0f25bbbc1beb2e856
Jibs005 <jayvieomongos8@gmail.com>
2025-09-08 10:01:39 +0800

    added mainmenu

--- factorial.js
+++ factorial.js
@@ -1,39 +1,42 @@
-const readline = require("readline");
+const readline = require("readline")
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+    input:process.stdin,
+    output:process.stdout
 
-console.log("Factorial Application");
-console.log("1. Say hello");
-console.log("2. Compute Factorial");
-console.log("3. Exit");
+})
+function mainMenu(){
+    console.log("Factorial Application");
 
-rl.question("Enter your choice (1-3): ", choice => {
-    if (choice === "1") {
-        console.log("Hello there!");
-        rl.close();
-    } else if (choice === "2") {
-        rl.question("Enter a number to compute the factorial: ", numstr => {
-            let num = parseInt(numstr);
+console.log ("factorial Application");
 
-            if (isNaN(num) || num < 0) {
-                console.log("Please enter a non-negative integer");
-            } else {
-                let fact = 1;
-                for (let i = 2; i <= num; i++) {
+console.log ("1. Say hello");
+console.log ("2.Compute Factorial");
+console.log ("3. Exit");
+
+rl.question("enter your choice (1-3)", choice =>{
+    if(choice==="1"){
+        console.log ("hello there!");
+    } else if (choice==="2"){
+        console.log ("factorial!");
+        rl.question("enter a number to the factorial", numstr =>{
+            
+            let num = parseint (numstr);
+
+            if(isNaN(num) || num < 0){
+                console.log("please enter a non-negative integer");
+            } else { 
+                let factorial = 1;
+                for (let i = 2; i <= num; i++){
                     fact *= i;
                 }
-                console.log(`The factorial of ${num} is ${fact}`);
+                console.log("the factorial of "+ num + " is " + fact);
             }
-            rl.close();
-        });
-    } else if (choice === "3") {
-        console.log("Exit!");
-        rl.close();
-    } else {
-        console.log("Invalid choice. Please try again");
-        rl.close();
+    } else if (choice==="3"){
+         console.log ("exciting programming!");
+         rl.close();
+    } else{
+        console.log("Invalid Choice.Please Try Again");
     }
-});
+
+})      
