44b196a54e18de4eb7bfaf834d71156b8bba06e5
Jibs005 <jayvieomongos8@gmail.com>
2025-09-18 08:25:21 +0800

    added class item

--- hero.js
+++ hero.js
@@ -5,16 +5,31 @@ class Hero{
         this.name = name;
         this.health = health;
         this.attack = attack;
+        this.item = [];
     }
     gethealth(){
       return this.#health;
   }
+
+    getName(){
+    console.log(this.name);
+}
+getAttack(){
+    console.log(this.attack);
+}
 
   getstatus(){
     console.log("name: " + this.name);
     console.log("health: " + this.health);
     console.log("attack: " + this.attack);
   }
+  addItem(item){
+    this.item.push(item);
+
+  }
+  totalAttack(){
+    return this.attack + this.item.reduce((sum, i) => sum + i.bonusAttack, 0);
+  } 
 
 }
 
@@ -32,16 +47,32 @@ class Mage extends Hero{
     useAbility(){
         console.log(`${this.name} use fireball`);
     }
+
 }
 
+class Item {
+    constructor(name, bonusAttack){
+        this.name = name;
+        this.bonusAttack = bonusAttack;
+    }
+}
+
+const sword = new Item("Sword", 5);
+const staff = new Item("Staff", 3);
+ 
 
 const Thorin= new Warrior("Thorin",100,10);
-Thorin.useAbility();
-Thorin.getstatus();
+Thorin.getName();
+Thorin.getAttack();
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+
 
 const Gandalf = new Mage("Gandalf",80,8,50);
-Gandalf.useAbility();
-Gandalf.getstatus();
+Gandalf.getName();
+Gandalf.getAttack();
+Gandalf.addItem(staff);
+console.log(Gandalf.totalAttack());
 
 function performAbility(hero){
     console.log("\n");
